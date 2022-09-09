d610b8d1b6aa34e2960b926e5bae9eac3b7db562
Dzhavat Ushev <dzhavatushev@gmail.com>
2022-09-09 23:37:57 +0200

    chore: delete page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-@Component({
-  template: `<h2>Page 1</h2>`,
-  standalone: true,
-})
-class Page1Component {}
-
-@Component({
-  template: `<h2>Page 2</h2>`,
-  standalone: true,
-})
-class Page2Component {}
-
-@Component({
-  template: `<h2>Page 3</h2>`,
-  standalone: true,
-})
-class Page3Component {}
-
-@Component({
-  template: `<h2>Page 4</h2>`,
-  standalone: true,
-})
-class Page4Component {}
-
 function generateComponent(content: string) {
   @Component({
     template: `<h2>{{ content }}</h2>`,
@@ -45,19 +21,19 @@ const routes: Routes = [
   },
   {
     path: 'page-1',
-    component: Page1Component,
+    component: generateComponent('Page 1'),
   },
   {
     path: 'page-2',
-    component: Page2Component,
+    component: generateComponent('Page 2'),
   },
   {
     path: 'page-3',
-    component: Page3Component,
+    component: generateComponent('Page 3'),
   },
   {
     path: 'page-4',
-    component: Page4Component,
+    component: generateComponent('Page 4'),
   },
 ];
 
